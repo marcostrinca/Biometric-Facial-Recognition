27d8f28cbaced7ea09c58d7322f416355999bad5
Marcos Trinca <marcostrinca@gmail.com>
2018-03-06 18:23:57 -0300

    using inception v2

--- face-recognition.py
+++ face-recognition.py
@@ -17,3 +17,124 @@ import pandas as pd
 import tensorflow as tf
 from fr_utils import *
 from inception_blocks_v2 import *
+
+FRmodel = faceRecoModel(input_shape=(3, 96, 96))
+print("Total Params:", FRmodel.count_params())
+
+def triplet_loss(y_true, y_pred, alpha = 0.2):
+    """
+    Implementation of the triplet loss as defined by formula (3)
+    
+    Arguments:
+    y_true -- true labels, required when you define a loss in Keras, you don't need it in this function.
+    y_pred -- python list containing three objects:
+            anchor -- the encodings for the anchor images, of shape (None, 128)
+            positive -- the encodings for the positive images, of shape (None, 128)
+            negative -- the encodings for the negative images, of shape (None, 128)
+    
+    Returns:
+    loss -- real number, value of the loss
+    """
+    
+    anchor, positive, negative = y_pred[0], y_pred[1], y_pred[2]
+    
+    # Step 1: Compute the (encoding) distance between the anchor and the positive, you will need to sum over axis=-1
+    pos_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, positive)), axis = -1)
+    # Step 2: Compute the (encoding) distance between the anchor and the negative, you will need to sum over axis=-1
+    neg_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, negative)), axis = -1)
+    # Step 3: subtract the two previous distances and add alpha.
+    basic_loss = tf.add(tf.subtract(pos_dist, neg_dist), alpha)
+    # Step 4: Take the maximum of basic_loss and 0.0. Sum over the training examples.
+    loss = tf.reduce_sum(tf.maximum(basic_loss, 0.0))
+    
+    return loss
+
+FRmodel.compile(optimizer = 'adam', loss = triplet_loss, metrics = ['accuracy'])
+load_weights_from_FaceNet(FRmodel)
+
+database = {}
+database["Camila"] = img_to_encoding("images/camila1.png", FRmodel)
+database["Marcos"] = img_to_encoding("images/marcos1.png", FRmodel)
+
+def verify(image_path, identity, database, model):
+    """
+    Function that verifies if the person on the "image_path" image is "identity".
+    
+    Arguments:
+    image_path -- path to an image
+    identity -- string, name of the person you'd like to verify the identity. Has to be a resident of the Happy house.
+    database -- python dictionary mapping names of allowed people's names (strings) to their encodings (vectors).
+    model -- your Inception model instance in Keras
+    
+    Returns:
+    dist -- distance between the image_path and the image of "identity" in the database.
+    door_open -- True, if the door should open. False otherwise.
+    """
+    
+    # Step 1: Compute the encoding for the image. Use img_to_encoding() see example above. (≈ 1 line)
+    encoding = img_to_encoding(image_path, model)
+    
+    # Step 2: Compute distance with identity's image (≈ 1 line)
+    dist = np.linalg.norm(database.get(identity) - encoding)
+    
+    # Step 3: Open the door if dist < 0.7, else don't open (≈ 3 lines)
+    if dist < 0.7:
+        print("It's " + str(identity) + ", welcome home!")
+        door_open = True
+    else:
+        print("It's not " + str(identity) + ", please go away")
+        door_open = False
+        
+    return dist, door_open
+
+print("Verification...")
+verify("images/camila2.png", "Camila", database, FRmodel)
+verify("images/camila3.png", "Camila", database, FRmodel)
+verify("images/camila4.png", "Camila", database, FRmodel)
+verify("images/marcos2.png", "Marcos", database, FRmodel)
+
+def who_is_it(image_path, database, model):
+    """
+    Implements face recognition for the happy house by finding who is the person on the image_path image.
+    
+    Arguments:
+    image_path -- path to an image
+    database -- database containing image encodings along with the name of the person on the image
+    model -- your Inception model instance in Keras
+    
+    Returns:
+    min_dist -- the minimum distance between image_path encoding and the encodings from the database
+    identity -- string, the name prediction for the person on image_path
+    """
+    
+    ## Step 1: Compute the target "encoding" for the image. Use img_to_encoding() see example above. ## (≈ 1 line)
+    encoding = img_to_encoding(image_path, model)
+    
+    ## Step 2: Find the closest encoding ##
+    
+    # Initialize "min_dist" to a large value, say 100 (≈1 line)
+    min_dist = 100
+    
+    # Loop over the database dictionary's names and encodings.
+    for (name, db_enc) in database.items():
+        
+        # Compute L2 distance between the target "encoding" and the current "emb" from the database. (≈ 1 line)
+        dist = np.linalg.norm(db_enc - encoding)
+
+        # If this distance is less than the min_dist, then set min_dist to dist, and identity to name. (≈ 3 lines)
+        if dist < 0.7:
+            min_dist = dist
+            identity = name
+    
+    if min_dist > 0.7:
+        print("Not in the database.")
+    else:
+        print ("it's " + str(identity) + ", the distance is " + str(min_dist))
+        
+    return min_dist, identity
+
+print("Who is it?")
+who_is_it("images/camila2.png", database, FRmodel)
+who_is_it("images/camila3.png", database, FRmodel)
+who_is_it("images/camila4.png", database, FRmodel)
+who_is_it("images/marcos2.png", database, FRmodel)
